44256aa41cb6f7f9cf537165a2ca5d0aaf8c2bd0
florence <loracodes3@gmail.com>
2025-01-29 00:50:41 +0300

    adding the student class model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,18 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
 from config import db
+from datetime import datetime
 
-# Models go here!
+
+# Student Model
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # One-to-many relationship with Enrollment
+   
