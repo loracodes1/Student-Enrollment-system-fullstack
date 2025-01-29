43d2292d54e9de852379b8b7bf98846218edf8db
florence <loracodes3@gmail.com>
2025-01-29 23:45:53 +0300

    adding the homepage data

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold">Welcome to the Student Enrollment System</h1>
+      </div>
+    );
+  }
+  
